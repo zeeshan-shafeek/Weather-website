6cf581e7b72c7518383e8801f3efa6ede7e2b00b
Zeeshan Shafique lap <zeeshaanshafique@gmail.com>
2022-11-14 17:56:37 +0500

    added search bar

--- app.py
+++ app.py
@@ -1,28 +1,35 @@
-from flask import Flask
+from flask import Flask, redirect, url_for, render_template, request
 import Weather
 
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('/', methods=["POST", "GET"])
 def home():
-    return '''
-    <h1>hello!</h1>
-    <h2>welcome to the this simple weather website</h2>
-    <p> Please enter the city in at the end of the link </p>
-    '''
-
-@app.route('/<city>')
-def get_weather(city):
-    w = Weather.WeatherApp(city, 'metric')
-    if w.status != 'City not found!':
-        return f'temp: {w.check_temp()} °C'
+    if request.method == "POST" and request.form["city"] != '' and request.form['unit'] != '':
+        city = request.form['city']
+        unit = request.form['unit']
+        return redirect(url_for("get_weather", city=city, unit=unit))
     else:
-        return f'<h1>{w.status}</h1>'
+        return render_template("search.html")
 
+    # return '''
+    # <h1>hello!</h1>
+    # <h2>welcome to the this simple weather website</h2>
+    # <p> Please enter the city in at the end of the link </p>
+    # '''
+
+
+@app.route('/<city>/<unit>')
+def get_weather(city, unit):
+    result = Weather.WeatherApp(city, unit)
+    if result.status != 'City not found!':
+        return render_template("results.html", city=city, unit=unit, temp=result.check_temp())
+    else:
+        return render_template("404page.html", city=city)
 
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
 
 # print("File r __name__ is set to: {}".format(__name__))
