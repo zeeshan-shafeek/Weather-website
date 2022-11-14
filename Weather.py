63635379486dc17971aa2ebf05202873f2fe6e6e
Zeeshan Shafique lap <zeeshaanshafique@gmail.com>
2022-11-14 12:06:11 +0500

    added Weather api

---
+++ Weather.py
@@ -0,0 +1,24 @@
+import requests
+from pprint import pprint
+
+
+class WeatherApp:
+    def __init__(self, city, units):
+        key = open("key.txt", "r")
+        self.api_key = key.read()
+        key.close()
+        self.status = ''
+        self.url = 'https://api.openweathermap.org/data/2.5/weather?q='
+        self.reply = {}
+        self.city = city
+        self.units = units
+        self.reply = requests.get(f"{self.url}{self.city}&appid={self.api_key}&units={self.units}")
+        if self.reply.status_code != 200:
+            self.status = 'City not found!'
+
+    def request_data(self):
+        self.reply = requests.get(f"{self.url}{self.city}&appid={self.api_key}&units={self.units}")
+
+    def check_temp(self):
+        self.request_data()
+        return self.reply.json()['main']['temp']
--- app.py
+++ app.py
@@ -1,16 +1,28 @@
 from flask import Flask
+import Weather
 
 app = Flask(__name__)
 
 
-@app.route('/home')
+@app.route('/')
 def home():
-    return 'hello! this is nice?'
+    return '''
+    <h1>hello!</h1>
+    <h2>welcome to the this simple weather website</h2>
+    <p> Please enter the city in at the end of the link </p>
+    '''
+
+@app.route('/<city>')
+def get_weather(city):
+    w = Weather.WeatherApp(city, 'metric')
+    if w.status != 'City not found!':
+        return f'temp: {w.check_temp()} °C'
+    else:
+        return f'<h1>{w.status}</h1>'
+
 
 
 if __name__ == "__main__":
     app.run()
 
 # print("File r __name__ is set to: {}".format(__name__))
-for _ in range(10):
-    print("hehe")
