de24860ab3142fb549854b6de9612346f426e89a
Zeeshan Shafique <zeeshan.shafeek@gmail.com>
2022-11-22 03:51:26 +0500

    added city CRUD, and dropdown

--- app.py
+++ app.py
@@ -25,17 +25,22 @@ def home():
         try:
             unit = request.form['unit']
         except KeyError:
-            flash("City added!")
-            return render_template("search.html")
-        return redirect(url_for("get_weather", city=city, unit=unit))
+            found_city = Cities.query.filter_by(cities=city).first()
+            if found_city:
+                flash("city already added!")
+            else:
+                test_city = Weather.WeatherApp(city, "imperial")
+                if test_city.status == 'City not found!':
+                    flash("Not a valid city")
+                    return render_template("search.html", city_list=Cities.query.all())
+                citi = Cities(city)
+                db.session.add(citi)
+                db.session.commit()
+                flash("City added!")
+            return render_template("search.html", city_list=Cities.query.all())
+        return redirect(url_for("get_weather", city=city, unit=unit, city_list=Cities.query.all()))
     else:
-        return render_template("search.html")
-
-    # return '''
-    # <h1>hello!</h1>
-    # <h2>welcome to the this simple weather website</h2>
-    # <p> Please enter the city in at the end of the link </p>
-    # '''
+        return render_template("search.html", city_list=Cities.query.all())
 
 
 @app.route('/<city>/<unit>', methods=["POST", "GET"])
@@ -43,23 +48,24 @@ def get_weather(city, unit):
     if request.method == "POST":
         city = request.form['city']
         unit = request.form['unit']
-        return redirect(url_for("get_weather", city=city, unit=unit))
+        return redirect(url_for("get_weather", city=city, unit=unit, city_list=Cities.query.all()))
 
     result = Weather.WeatherApp(city, unit)
     if result.status != 'City not found!':
-        return render_template("results.html", city=city, unit=unit, temp=result.check_temp())
+        return render_template("results.html", city=city, unit=unit, temp=result.check_temp(), city_list=Cities.query.all())
     else:
-        return render_template("404page.html", city=city)
+        return render_template("404page.html", city=city, city_list=Cities.query.all())
 
 
 @app.route('/addcity/', methods=["POST", "GET"])
 def add_city():
     session.pop('_flashes', None)
-    return render_template("addcity.html")
+    return render_template("addcity.html", city_list=Cities.query.all())
 
 
 if __name__ == "__main__":
+    with app.app_context():
+        db.create_all()
     app.run(debug=True)
-    db.create_all()
 
 # print("File r __name__ is set to: {}".format(__name__))
